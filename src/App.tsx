db8b44e9b77fd99f6de2be2f4ae2f2b0237c9565
Alain Lepage <alain.lepage@gmail.com>
2023-12-15 23:55:22 +0000

    version 0.5.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useState } from 'react';
 import { Jupyter, Notebook, CellSidebarRun } from '@datalayer/jupyter-react';
 import { Box } from '@primer/react';
-import { AppsIcon, CpuIcon } from '@primer/octicons-react';
-import { UnderlineNav } from '@primer/react/drafts';
-import CellComponents from './examples/cell/CellComponents';
-import OutputsComponents from './examples/outputs/OutputsComponents';
-import JupyterLabHeadlessApp from './examples/labapp/JupyterLabHeadlessApp';
 
 import './App.css';
 
 const App = () => {
-  const [tab, setTab] = useState(0);
+
   return (
     <>
       <Box>
